1e7c0bacdd9cf8cb1577f4fab671d43c2128c898
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2019-11-25 02:46:56 -0300

    Criando um novo modelo
    dando o nome do capitulo como commit para facilitar na hora de procurar no livro qual parte foi esse commit, seguindo capitulos 5.1, 5.2 e 5.3

--- modeloV2/app/controllers/NegociacaoController.js
+++ modeloV2/app/controllers/NegociacaoController.js
@@ -2,26 +2,33 @@ class NegociacaoController {
 
     constructor() {
 
+
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes()
     }
 
     adiciona(event) {
-
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        this._negociacoes.adiciona(this._criaNegociacao())
+
+        this._limparFormulario()
+    }
+
+    _limparFormulario() {
+        this._inputData.value = ''
+        this._inputQuantidade.value = 1
+        this._inputValor.value = 0.0
+        this._inputData.focus()
+    }
+    _criaNegociacao() {
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-
-        console.log(negociacao.data);
-        
-        
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-        console.log(diaMesAno);
     }
 }
