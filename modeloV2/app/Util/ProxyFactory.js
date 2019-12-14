1005da6fb1c7d946f00f874d61b301e46e5d975e
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2019-12-14 14:09:32 -0300

    update ProxyFactory

--- modeloV2/app/Util/ProxyFactory.js
+++ modeloV2/app/Util/ProxyFactory.js
@@ -1,8 +1,9 @@
 class ProxyFactory {
   static create(objeto, props, armadilha) {
+    // recebe um objeto como parâmetro
     return new Proxy(objeto, {
       get(target, prop, receiver) {
-        if (typeof target[prop] == typeof Function && props.includes(prop)) {
+        if (ProxyFactory._ehFuncao(target[prop] && props.includes(prop)) {
           return function() {
             console.log(`"${prop}" disparou a armadilha`);
             target[prop].apply(target, arguments);
@@ -12,7 +13,16 @@ class ProxyFactory {
         } else {
           return target[prop];
         }
+      },
+      set(target, prop, value, receiver) {
+        const updated = Reflect.set(target, prop, value);
+        if (props.includes(prop)) armadilha(target);
+        return updated;
       }
     });
+  }
+
+  static _ehFuncao(fn) {
+    return typeof fn == typeof Function;
   }
 }
